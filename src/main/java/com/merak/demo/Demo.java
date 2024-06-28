998b3c3c42cfe6ae6814b64abebd4116553ca304
喷火娃 <1272659327@qq.com>
2024-06-28 21:24:17 +0800

    init

---
+++ src/main/java/com/merak/demo/Demo.java
@@ -0,0 +1,8 @@
+package com.merak.demo;
+
+public class Demo {
+
+    public static void fun(){
+        System.out.println("*****************");
+    }
+}
---
+++ src/test/java/com/merak/demo/DemoTest.java
@@ -0,0 +1,13 @@
+package com.merak.demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DemoTest {
+
+    @Test
+    void fun() {
+        Demo.fun();
+    }
+}
